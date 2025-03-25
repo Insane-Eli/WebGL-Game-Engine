65ec531a3d8ad619baa74451437cb6cca56a445b
Eli <eli@currah.com>
2025-03-24 20:38:08 -0700

    6.3 done yabish

---
+++ Chapters/CH6/CH6.3/src/engine/core/shader_resources.js
@@ -0,0 +1,62 @@
+"use strict";
+import SimpleShader from "../shaders/simple_shader.js";
+import * as text from "../resources/text.js";
+import * as map from "./resource_map.js";
+import TextureShader from "../shaders/texture_shader.js";
+import SpriteShader from "../shaders/sprite_shader.js";
+
+let mSpriteShader = null;
+function getSpriteShader() { return mSpriteShader; }
+
+// Simple Shader
+let kSimpleVS = "src/glsl_shaders/simple_vs.glsl"; // to VertexShader
+let kSimpleFS = "src/glsl_shaders/simple_fs.glsl"; // to FragmentShader
+let mConstColorShader = null;
+
+// Texture Shader
+let kTextureVS = "src/glsl_shaders/texture_vs.glsl"; // VertexShader
+let kTextureFS = "src/glsl_shaders/texture_fs.glsl"; // FragmentShader
+let mTextureShader = null;
+
+function createShaders() {
+  mConstColorShader = new SimpleShader(kSimpleVS, kSimpleFS);
+  mTextureShader = new TextureShader(kTextureVS, kTextureFS);
+  mSpriteShader = new SpriteShader(kTextureVS, kTextureFS);
+}
+
+// this is private/protected
+function _setShader(s) { this.mShader = s; } //WAS I SUPPOSED TO DO THISs
+
+function init() {
+  let loadPromise = new Promise(async function (resolve) {
+    await Promise.all([
+      text.load(kSimpleFS),
+      text.load(kSimpleVS),
+      text.load(kTextureFS),
+      text.load(kTextureVS)
+    ]);
+    resolve();
+  }).then(function resolve() {
+    createShaders();
+  });
+  map.pushPromise(loadPromise);
+}
+
+function cleanUp() {
+  mConstColorShader.cleanUp();
+  mTextureShader.cleanUp();
+  mSpriteShader.cleanUp();
+  text.unload(kSimpleVS);
+  text.unload(kSimpleFS);
+  text.unload(kTextureVS);
+  text.unload(kTextureFS);
+}
+function getConstColorShader() {
+  return mConstColorShader;
+}
+
+function getTextureShader() { return mTextureShader; }
+
+
+
+export { init, cleanUp, getConstColorShader, getTextureShader, getSpriteShader };
---
+++ Chapters/CH6/CH6.3/src/my_game/my_game.js
@@ -0,0 +1,133 @@
+"use strict";
+import engine from "../engine/index.js";
+// user stuff
+import DyePack from "../engine/game_objects/dye_pack.js";
+import Minion from "../engine/game_objects/minion.js";
+import Hero from "../engine/game_objects/hero.js";
+import Brain from "../engine/game_objects/brain.js";
+
+class MyGame extends engine.Scene {
+  constructor() {
+    super();
+    // textures:
+    this.kFontImage = "assets/consolas-72.png";
+    this.kMinionSprite = "assets/minion_sprite.png";
+
+    // The camera to view the scene
+    this.mCamera = null;
+    // the hero and the support objects
+    this.mHero = null;
+    // this.mDyePack = null;
+    // this.mMinionset = null;
+    this.mMsg = null;
+    this.mBrain = null;
+  }
+
+  init() {
+    // Step A: set up the cameras and set the background to gray
+    this.mCamera = new engine.Camera(
+      vec2.fromValues(50, 37.5), // position of the camera
+      100, // width of camera
+      [0, 0, 640, 480] // viewport (orgX, orgY, width, height)
+    );
+    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+    // Step B: The dye pack: simply another GameObject
+
+    // this.mDyePack = new DyePack(this.kMinionSprite);
+    // // Step C: A set of Minions
+    // this.mMinionset = new engine.GameObjectSet();
+    // let i = 0, randomY, aMinion;
+    // // create 5 minions at random Y values
+    // for (i = 0; i < 5; i++) {
+    // randomY = Math.random() * 65;
+    // aMinion = new Minion(this.kMinionSprite, randomY);
+    // this.mMinionset.addToSet(aMinion);
+    // }
+
+    // Step D: Create the hero object
+    this.mHero = new Hero(this.kMinionSprite);
+    // Step E: Create and initialize message output
+    this.mMsg = new engine.FontRenderable("Status Message");
+    this.mMsg.setColor([0, 0, 0, 1]);
+    this.mMsg.getXform().setPosition(1, 2);
+    this.mMsg.setTextHeight(3);
+    // Step F: add the brain shart
+    this.mBrain = new Brain(this.kMinionSprite);
+  }
+  draw() {
+    // Step A: clear the canvas
+    engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+    // Step B: Activate the drawing Camera
+    this.mCamera.setViewAndCameraMatrix();
+    // Step C: Draw everything
+    this.mHero.draw(this.mCamera);
+    this.mMsg.draw(this.mCamera);
+    // this.mDyePack.draw(this.mCamera);
+    // this.mMinionset.draw(this.mCamera);
+    this.mBrain.draw(this.mCamera);
+  }
+  update() {
+    let msg = "Brain [H:keys J:imm K:gradual]: ";
+    let rate = 1;
+    this.mHero.update();
+    let hBbox = this.mHero.getBBox();
+    let bBbox = this.mBrain.getBBox();
+    switch (this.mMode) {
+      case 'H':
+        this.mBrain.update(); // player steers with arrow keys
+        break;
+      case 'K':
+        rate = 0.02; // gradual rate
+      // In gradual mode, the following should also be executed
+      case 'J':
+        // stop the brain when it touches hero bound
+        if (!hBbox.intersectsBound(bBbox)) {
+          this.mBrain.rotateObjPointTo(
+            this.mHero.getXform().getPosition(), rate);
+          // the default GameObject: only move forward
+          engine.GameObject.prototype.update.call(this.mBrain);
+        }
+        break;
+    }
+    // Check for hero going outside 80% of the WC Window bound
+    let status = this.mCamera.collideWCBound(this.mHero.getXform(), 0.8);
+    if (engine.input.isKeyClicked(engine.input.keys.H)) {
+      this.mMode = 'H';
+    }
+    if (engine.input.isKeyClicked(engine.input.keys.J)) {
+      this.mMode = 'J';
+    }
+    if (engine.input.isKeyClicked(engine.input.keys.K)) {
+      this.mMode = 'K';
+    }
+    this.mMsg.setText(msg + this.mMode + " [Hero bound=" + status + "]");
+  }
+  load() {
+    // Step A: loads the textures
+    engine.texture.load(this.kFontImage);
+    engine.texture.load(this.kMinionSprite);
+  }
+
+  unload() {
+    engine.texture.unload(this.kFontImage);
+    engine.texture.unload(this.kMinionSprite);
+  }
+
+
+  _initText(font, posX, posY, color, textH) {
+    font.setColor(color);
+    font.getXform().setPosition(posX, posY);
+    font.setTextHeight(textH);
+  }
+
+}
+
+
+
+window.onload = function () {
+  engine.init("GLCanvas");
+  let myGame = new MyGame();
+  myGame.start();
+};
+
+export default MyGame;
