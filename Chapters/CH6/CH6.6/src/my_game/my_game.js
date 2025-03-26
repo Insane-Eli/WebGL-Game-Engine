7e1460fb55a9c9140a2064863107a02764414be7
Eli <eli@currah.com>
2025-03-26 12:25:27 -0700

    got everything laid out for 6.6 + fixed index.html
    to be accurate for each chapter

--- Chapters/CH6/CH6.6/src/my_game/my_game.js
+++ Chapters/CH6/CH6.6/src/my_game/my_game.js
@@ -6,6 +6,8 @@ import Minion from "../engine/game_objects/minion.js";
 import Hero from "../engine/game_objects/hero.js";
 import Brain from "../engine/game_objects/brain.js";
 import TextureObject from "../engine/game_objects/texture_object.js";
+import SpriteRenderable from "../engine/renderables/sprite_renderable.js";
+import SpriteAnimateRenderable from "../engine/renderables/sprite_animate_renderable.js";
 class MyGame extends engine.Scene {
   constructor() {
     super();
@@ -13,7 +15,7 @@ class MyGame extends engine.Scene {
     this.kFontImage = "assets/consolas-72.png";
     this.kMinionSprite = "assets/minion_sprite.png";
     this.kMinionPortal = "assets/minion_portal.png";
-    this.kMinionCollector = "assets/minion_collector.png";
+   // this.kMinionCollector = "assets/minion_collector.png";
 
     // The camera to view the scene
     this.mCamera = null;
@@ -23,7 +25,7 @@ class MyGame extends engine.Scene {
     this.mPortal = null;
     this.mBrain = null;
     this.mLeftMinion = null;
-    this.mLeftMinion = null;
+    this.mRighttMinion = null;
 
   }
 
@@ -37,64 +39,88 @@ class MyGame extends engine.Scene {
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     // sets the background to gray
 
+    this.mMsg = new engine.FontRenderable("Status Message");
+    this.mMsg.setColor([0, 0, 0, 1]);
+    this.mMsg.getXform().setPosition(1, 2);
+    this.mMsg.setTextHeight(2);
+
     this.mPortal = new TextureObject(this.kMinionPortal, 70, 30, 10, 10);
 
     this.mHero = new Hero(this.kMinionSprite);
 
-    this.mCollector = new TextureObject(this.kMinionCollector,
-      50, 30, 30, 30);
+    this.mBrain = new Brain(this.kMinionSprite)
 
-    this.mMsg = new engine.FontRenderable("Status Message");
-    this.mMsg.setColor([0, 0, 0, 1]);
-    this.mMsg.getXform().setPosition(1, 2);
-    this.mMsg.setTextHeight(3);
+    this.mLeftMinion = new Minion(this.kMinionSprite)
+    this.mRightMinion = new Minion(this.kMinionSprite)
+
+
+    // this.mCollector = new TextureObject(this.kMinionCollector,
+    //   50, 30, 30, 30);
+
   }
   draw() {
     // Step A: clear the canvas
     engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     // Step B: Activate the drawing Camera
     this.mCamera.setViewAndCameraMatrix();
+    this.mMsg.draw(this.mCamera);
     // Step C: Draw everything
     this.mHero.draw(this.mCamera);
-    this.mMsg.draw(this.mCamera);
     this.mPortal.draw(this.mCamera);
-    this.mCollector.draw(this.mCamera);
-
-    // this.mMinionset.draw(this.mCamera);
-    //this.mBrain.draw(this.mCamera);
+    this.mLeftMinion.draw(this.mCamera);
+    this.mRightMinion.draw(this.mCamera);
+    this.mBrain.draw(this.mCamera);
   }
+
   update() {
-    let msg = "No Collision";
-    this.mHero.update(engine.input.keys.W, engine.input.keys.S,
-      engine.input.keys.A, engine.input.keys.D);
+
+    // reqs:
+    // DONE: Arrow and P keys: Move and rotate the Portal minion
     this.mPortal.update(engine.input.keys.Up, engine.input.keys.Down,
       engine.input.keys.Left, engine.input.keys.Right, engine.input.keys.P);
+
+    // DONE: WASD keys: Move the Hero
+    this.mHero.update(engine.input.keys.W, engine.input.keys.S,
+      engine.input.keys.A, engine.input.keys.D);
+
+    // L, R, H, B keys: Select the target for colliding with the Portal minion
+      
+      // pressing l and r will change the collision detection between the portal minion
+      
+      // pressing h will do the same with the hero
+      
+      // pressing b will do the same with the brain
+
+    // the brain object constantly seeks out the hero
+
+    let target = this.mBrain;
+    let msg = "WASD target:" + target + " [L:left minion R:right minion H:hero B:brain] ";
+
     let h = [];
     // Portal's resolution is 1/16 x 1/16 that of Collector!
     // VERY EXPENSIVE!!
+      
     // if (this.mCollector.pixelTouches(this.mPortal, h)) {
-    if (this.mPortal.pixelTouches(this.mCollector, h)) {
-
+    if (this.mPortal.pixelTouches(target, h)) {
       msg = "Collided!: (" + h[0].toPrecision(4) + " " +
         h[1].toPrecision(4) + ")";
-
     }
+
     this.mMsg.setText(msg);
   }
-
 
   load() {
     // Step A: loads the textures
     engine.texture.load(this.kFontImage);
     engine.texture.load(this.kMinionSprite);
-    engine.texture.load(this.kMinionCollector);
+    //engine.texture.load(this.kMinionCollector);
     engine.texture.load(this.kMinionPortal);
   }
 
   unload() {
     engine.texture.unload(this.kFontImage);
     engine.texture.unload(this.kMinionSprite);
-    engine.texture.unload(this.kMinionCollector);
+    //engine.texture.unload(this.kMinionCollector);
     engine.texture.unload(this.kMinionPortal);
   }
 
